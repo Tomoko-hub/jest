9402ea8a47e59de81eb49770d0d6cdc63a6cea9c
Tomoko-hub <haisaitomoco@gmail.com>
2024-07-30 15:34:15 +0300

    add test.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import TodoTable from './TodoTable';
 import './App.css';
 
 function App() {
+/*   const [ todo, setTodo ] = useState({description: '', date: ''});
+  const [ todos, setTodos ] = useState([]);
+
+  const inputChanged =(event)=>{
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  const addTodo =()=>{
+    setTodos([...todos, todo]);
+    setTodo({description: '', date:''})
+  }
+
+  const deleteTodo=(row)=>{
+    setTodos(todos.filter((todo, index)=>index !== row));
+  } */
+
+    const [ userInfo, setUserInfo ] = useState({firstname: '', lastname: '', email: '', phone: ''});
+
+    const inputChanged=(event)=>{
+      setUserInfo({...userInfo, [event.target.name]: event.target.value});
+    }
+
+    const handleSubmit =(event)=>{
+      event.preventDefault();
+      const { firstname, lastname, email, phone } = userInfo;
+      if(!firstname|| !lastname || !email || !phone){
+        alert('All fields are reauired.')
+      } else {
+        alert(`Welcome ${userInfo.firstname} ${userInfo.lastname}`);
+        setUserInfo({
+          firstname: '',
+          lastname: '',
+          email: '',
+          phone: ''
+        });
+      }
+    }
+
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <input placeholder='Description' name="description" value={todo.description} onChange={inputChanged} />
+      <input placeholder='Date' name="date" value={todo.date} onChange={inputChanged} />
+      <button onClick={addTodo}>Add</button>
+      <TodoTable todos={todos} deleteTodo={deleteTodo}  /> */}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <input placeholder='First name' name='firstname' value={userInfo.firstname} onChange={inputChanged} />
+          </label>
+        </div>
+        <div>
+          <label>
+            <input placeholder='Last name' name='lastname' value={userInfo.lastname} onChange={inputChanged} />
+          </label>
+        </div>
+        <div>
+          <label>
+            <input placeholder='Email' name='email' value={userInfo.email} onChange={inputChanged} />
+          </label>
+        </div>
+        <div>
+          <label>
+            <input placeholder='Phone' name='phone' value={userInfo.phone} onChange={inputChanged} />
+          </label>
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import TodoTable from "./TodoTable";
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
-test('renders learn react link', () => {
+test('renders todotable',()=>{
+  const row=[
+    {description:'Go to coffee', date:'24.11.2022'}
+  ]
+
+  render(<TodoTable todos={row} />);
+  const tableRow = screen.getByText('Go to coffee');
+  expect(tableRow).toBeInTheDocument();
+})
+
+test('add todo', ()=>{
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+  const desc = screen.getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: { value: 'Go to coffee'}})
+  const date = screen.getAllByPlaceholderText('Date');
+  fireEvent.change(date, { target: { value: '29.11.2020'}})
+
+  fireEvent.click(screen.getByText('Add'));
+  expect(screen.getByText('Go to coffee')).toBeInTheDocument();
+})
